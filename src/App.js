7d4a9b2a43af6b0620bb892ff9be1f30fdeb4f48
Bryson Nathan Lan <nathanblan.8@gmail.com>
2021-04-03 20:43:32 -0700

    add tutorial code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 
 function App() {
   return (
     <div className="App">
-      
+      <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
 }
